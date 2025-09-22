7e0301b72ec2aaa367c1531088ec1ff37e22ea8c
Lordkyllua <117127321+Lordkyllua@users.noreply.github.com>
2025-09-21 21:49:10 -0300

    Actualizar clan.js

--- commands/clan.js
+++ commands/clan.js
@@ -1,9 +1,29 @@
-const { EmbedBuilder } = require('discord.js');
+const { ActionRowBuilder, StringSelectMenuBuilder, EmbedBuilder } = require("discord.js");
+
 module.exports = {
-  name: 'clan',
-  description: 'Clan menu',
-  execute(message) {
-    const embed = new EmbedBuilder().setTitle('🏰 Clan Menu').setColor('Gold').setDescription('Use: !rpg createclan/joinclan/leaveclan/claninfo').setFooter({ text: 'Developed by LordK' });
-    message.channel.send({ embeds: [embed] });
-  }
+    name: "clan",
+    description: "Open the Clan menu",
+    async execute(message) {
+        const options = [
+            { label: "Create Clan", value: "create" },
+            { label: "Join Clan", value: "join" },
+            { label: "Leave Clan", value: "leave" },
+            { label: "Clan Info", value: "info" },
+            { label: "Invite Member", value: "invite" },
+        ];
+
+        const selectMenu = new StringSelectMenuBuilder()
+            .setCustomId("clan_select")
+            .setPlaceholder("Choose a Clan action")
+            .addOptions(options);
+
+        const row = new ActionRowBuilder().addComponents(selectMenu);
+
+        const embed = new EmbedBuilder()
+            .setTitle("🏰 Clan Menu")
+            .setDescription("Select a clan action from the menu below.")
+            .setColor("Gold");
+
+        message.channel.send({ embeds: [embed], components: [row] });
+    },
 };
