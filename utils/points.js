f053898dbc21caf2b73db1f0cde0a0a5460f8052
Lordkyllua <117127321+Lordkyllua@users.noreply.github.com>
2025-09-23 06:54:39 -0300

    Add files via upload

--- utils/points.js
+++ utils/points.js
@@ -1,7 +1,7 @@
 
 const fs = require('fs');
 const PATH = './points.json';
-function load(){ try { return JSON.parse(fs.readFileSync(PATH,'utf8')); } catch(e){ return {}; } }
+function load(){ try{ return JSON.parse(fs.readFileSync(PATH,'utf8')); }catch(e){ return {}; } }
 function save(db){ fs.writeFileSync(PATH, JSON.stringify(db, null, 2)); }
 module.exports = {
   addPoints(userId, amount){ const db=load(); if(!db[userId]) db[userId]={points:0,lastDaily:0}; db[userId].points += amount; save(db); return db[userId].points; },
